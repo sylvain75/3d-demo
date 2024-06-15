6a11444192da629be00fefebae8e280fcfc46eeb
Sylvain <sylvoz75@gmail.com>
2024-06-15 11:35:26 +1000

    tailwind + basic sphere / mesh scene

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import "../app.css";
+</script>
+
+<div class="scene">
+	<slot />
+</div>
+
+<svelte:head>
+	<title>Threlte</title>
+</svelte:head>
+
+<style>
+  .scene {
+    position: absolute;
+    inset: 0;
+    background: radial-gradient(hsl(220 14% 20%), hsl(220 20% 10%));
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script lang="ts">
+	import { Canvas } from '@threlte/core'
+	import Scene from './scene.svelte'
+</script>
+
+<Canvas>
+<h1 class="text-2xl text-amber-400">Best goon in town</h1>
+	<Scene />
+</Canvas>
